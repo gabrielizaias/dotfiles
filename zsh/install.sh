e70a4af5687457ece7f08d112cbe8f9f2450a267
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-12-22 00:18:39 -0200

    Add zsh to /etc/shells and make it default shell

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,3 +9,11 @@ else
     msg_info "Installing Zsh (via Homebrew)"
     brew install zsh
 fi
+
+# Add zsh to /etc/shells
+msg_info "Adding zsh to /etc/shells"
+sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+
+# Set zsh as default shell
+msg_info "Making zsh the default shell"
+chsh -s /usr/local/bin/zsh $USER
