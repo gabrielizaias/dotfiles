1d82222801e3ebc62436bfa8e1398793129a6ab8
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-12-21 23:21:38 -0200

    Add symlink_dotfiles function

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,8 +17,62 @@ fi
 # Find the installers and run them iteratively
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
 
+function symlink_dotfiles {
+    msg_info 'Linking dotfiles'
 
-function link_files {
-    ln -s $1 $2
-    success "Linked $1 to $2"
+    overwrite_all=false
+    backup_all=false
+    skip_all=false
+
+    for source in `find $HOME/.dotfiles -maxdepth 2 -name \*.symlink`; do
+        dest="$HOME/.`basename ${source%.*}`"
+
+        if [ -f $dest ] || [ -d $dest ]; then
+            overwrite=false
+            backup=false
+            skip=false
+
+            if ! $overwrite_all && ! $backup_all && ! $skip_all; then
+                msg_warning "File already exists: `basename $source`, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+                read -n 1 action
+
+                case "$action" in
+                    o )
+                        overwrite=true;;
+                    O )
+                        overwrite_all=true;;
+                    b )
+                        backup=true;;
+                    B )
+                        backup_all=true;;
+                    s )
+                        skip=true;;
+                    S )
+                        skip_all=true;;
+                    * )
+                        ;;
+                esac
+            fi
+
+            if $overwrite || $overwrite_all; then
+                rm -rf $dest
+                msg_success "Removed $dest"
+            fi
+
+            if $backup || $backup_all; then
+                mv $dest $dest\.backup
+                msg_success "Moved $dest to $dest.backup"
+            fi
+
+            if ! $skip && ! $skip_all; then
+                link_files $source $dest
+            else
+                msg_success "Skipped $source"
+            fi
+        else
+            link_files $source $dest
+        fi
+    done
 }
+
+symlink_dotfiles
