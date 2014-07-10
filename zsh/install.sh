bd842f1c3d84042b4bd03094db82c4707bc27a8e
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-07-10 11:17:34 -0300

    Only add zsh to shells when installing it

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,12 +8,13 @@ if which zsh &> /dev/null; then
 else
     msg_info "Installing Zsh (via Homebrew)"
     brew install zsh
+
+    # Add zsh to /etc/shells
+    msg_info "Adding zsh to /etc/shells"
+    sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+
+    # Set zsh as default shell
+    msg_info "Making zsh the default shell"
+    chsh -s /usr/local/bin/zsh $USER
 fi
 
-# Add zsh to /etc/shells
-msg_info "Adding zsh to /etc/shells"
-sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-
-# Set zsh as default shell
-msg_info "Making zsh the default shell"
-chsh -s /usr/local/bin/zsh $USER
