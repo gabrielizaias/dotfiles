c8cdf1d65d130c1645157873d1a3d76c8b31ccf3
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-07-10 14:52:04 -0300

    Add uninstall script
    Basically unlink every `*.symlink` from the `$HOME` folder

---
+++ uninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit script if an error occurs
+set -e
+
+# Utility functions
+source utils.sh
+
+msg_info "Unlinking dotfiles"
+
+for source in `find $HOME/.dotfiles -maxdepth 2 -name \*.symlink`; do
+    dest="$HOME/.`basename ${source%.*}`"
+    if [ -f $dest ] || [ -d $dest ]; then
+        unlink $dest
+        msg_success "Unlinked $dest"
+    fi
+done
+
+msg "Done"
