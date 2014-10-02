740b7b3f1e215fdbe01355c753ad5792b3e7e6e0
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-10-02 19:27:49 -0300

    path: Remove path to CakePHP binary inside MAMP

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
 # Add .rvm/bin to PATH
-PATH="/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:/Applications/MAMP/htdocs/cakephp/lib/Cake/Console/:$PATH"
+PATH="/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
