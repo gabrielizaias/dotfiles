052a6a08ba737caf8cd1730f54c1440b317864fd
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-10-22 15:42:48 -0200

    path: Add composer /bin folder to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
 # Add .rvm/bin to PATH
-PATH="/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
+PATH="/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/.composer/vendor/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
