2534f572e367621ff8d773d9baf7ffc2b21aeee9
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-06-15 00:51:43 -0300

    .path: Add homebrews php56 bin to PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
 # Add .rvm/bin to PATH
-PATH="/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/.composer/vendor/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
+PATH="$(brew --prefix homebrew/php/php56)/bin:/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/.composer/vendor/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
