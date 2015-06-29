dc3f03fa95d86586a3840a9bc17aaf2c6714bf65
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-06-28 22:55:33 -0300

    git/functions: Make `gitexport` work with the current branch instead of master

--- git/functions.zsh
+++ git/functions.zsh
@@ -1,7 +1,7 @@
 # take this repo and copy it to somewhere else minus the .git stuff.
 function gitexport() {
   mkdir -p "$1"
-  git archive master | tar -x -C "$1"
+  git archive $(git rev-parse --abbrev-ref HEAD) | tar -x -C "$1"
 }
 
 # generate .gitignore automatically using gitignore.io
