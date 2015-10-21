e113d8020ad1ba41913ec9706f063b0b4a164d0c
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-10-21 13:10:53 -0200

    path: Add makeshift path to nvm node bin

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,5 @@
 # Add .rvm/bin to PATH
 PATH="$(brew --prefix homebrew/php/php56)/bin:/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/.composer/vendor/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
+
+#export NVM_DIR="/Users/gabriel/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
