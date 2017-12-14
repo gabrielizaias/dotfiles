abf0d0476a994039cc8a2fcd3a0e14808c2229b9
Gabriel Izaias <gabriel.izaias@gmail.com>
2017-12-14 13:52:47 +0100

    path: Fix nvm loading

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,6 @@
 # Add .rvm/bin to PATH
 PATH="$(brew --prefix homebrew/php/php72)/bin:$HOME/.composer/vendor/bin:/usr/local/opt/sqlite/bin:/usr/local/bin:/usr/local/sbin:~/bin:~/.rvm/bin:~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH"
 
-#export NVM_DIR="/Users/gabriel/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Load nvm
+export NVM_DIR="$(brew --prefix nvm)"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
