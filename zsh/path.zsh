e27dbfbf5dc3fb487a44642cf41541b3ae3bf61d
Gabriel Izaias <gabriel.izaias@gmail.com>
2019-03-06 15:04:57 +0100

    path: Add deno bin to $PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,5 @@
 # Add .rvm/bin to PATH
-PATH="$(brew --prefix php)/bin:$HOME/.composer/vendor/bin:/usr/local/opt/sqlite/bin:/usr/local/opt/ruby/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.rvm/bin:$PATH"
+PATH="$(brew --prefix php)/bin:$HOME/.deno/bin:$HOME/.composer/vendor/bin:/usr/local/opt/sqlite/bin:/usr/local/opt/ruby/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.rvm/bin:$PATH"
 
 # Load nvm
 export NVM_DIR="$(brew --prefix nvm)"
