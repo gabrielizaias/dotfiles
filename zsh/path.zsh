8b4d9a1ac79d4d7debceafd5563cc5022cf18d33
Gabriel Izaias <gabriel.izaias@gmail.com>
2021-05-04 20:36:14 +0200

    path: cleanup zsh path config

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,9 +1,9 @@
-# Add .rvm/bin to PATH
 PATH="$(brew --prefix php)/bin:$HOME/.deno/bin:$HOME/.composer/vendor/bin:/usr/local/opt/sqlite/bin:/usr/local/opt/ruby/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.rvm/bin:$PATH"
 
 # Load nvm
-export NVM_DIR="$(brew --prefix nvm)"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # Fix gpg error
 # @see https://tutorials.technology/solved_errors/21-gpg-signing-failed-Inappropriate-ioctl-for-device.html
